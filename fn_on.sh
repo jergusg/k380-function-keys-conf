bf4cbc279b6c34f2ccac1720e49aac690a8e77d6
Yihui Xiong <xiongyihui3@gmail.com>
2017-06-06 20:22:02 +0800

    bugfix

--- fn_on.sh
+++ fn_on.sh
@@ -2,13 +2,13 @@
 
 
 TOOL="$(dirname $0)/k380_conf"
-DEV="/dev/$(ls /sys/class/hidraw/ -l | grep 046D:B342 | grep -o 'hidraw[0-9]*$')"
+DEV="$(ls /sys/class/hidraw/ -l | grep 046D:B342 | grep -o 'hidraw[0-9]*$')"
 
 
 if test -n "$DEV" 
 then
     echo "Found K380 at $DEV"
-    $TOOL -d $DEV -f on
+    $TOOL -d "/dev/$DEV" -f on
 fi
 
 
