19e41c31a19d2c0eacad04171d6f9a90a33b2a02
Yihui Xiong <yihui.xiong@hotmail.com>
2017-07-03 09:42:46 +0800

    set fn on only when k380 is detected

--- fn_on.sh
+++ fn_on.sh
@@ -5,7 +5,7 @@ TOOL="$(dirname $0)/k380_conf"
 DEV="$(ls /sys/class/hidraw/ -l | grep 046D:B342 | grep -o 'hidraw[0-9]*$')"
 
 
-if test -n "$DEV" 
+if test -n "$DEV" && (test -z "$1" || test "/dev/$DEV" = "$1")
 then
     echo "Found K380 at $DEV"
     $TOOL -d "/dev/$DEV" -f on
